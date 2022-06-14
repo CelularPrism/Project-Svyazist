4d376857b55bca61594ade9c1c4804dca836f735
LiubovPil <alexandraelle95@gmail.com>
2022-06-14 19:55:21 +0300

    Add script for enemy animator

--- Assets/Scripts/Enemy/Movement/EnemyAnimator.cs
+++ Assets/Scripts/Enemy/Movement/EnemyAnimator.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class EnemyAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Required components")]
+    [SerializeField] private Animator _enemyAnimator;
+
+    [Header("Options for Idle animation")]
+    [SerializeField] private AnimationClip[] _idleAnimations;
+
+    #region Names (string) of parametres
+    private string _nameIdleParameter = "Idle";
+    private string _nameBlendTreeParameter = "BlendTree";
+    private string _nameDetectedParameter = "Detected";
+    private string _nameLostGoalParameter = "LostGoal";
+    private string _nameCanShootParameter = "CanShoot";
+    #endregion
+
+    private float[] _idleIndex = { 0, 1 };
+    private int _currentIdleNum;
+
+
+    private void Awake()
     {
+        _enemyAnimator = GetComponent<Animator>();
+
+        _enemyAnimator.ResetTrigger(_nameDetectedParameter);
+        _enemyAnimator.ResetTrigger(_nameLostGoalParameter);
+        _enemyAnimator.ResetTrigger(_nameCanShootParameter);
         
+        _enemyAnimator.SetFloat(_nameBlendTreeParameter, 0);
+
+        Idle();
+    }
+    public void Idle()
+    {
+        _currentIdleNum = Random.Range(0, _idleAnimations.Length);
+        _enemyAnimator.SetFloat(_nameIdleParameter, _idleIndex[_currentIdleNum]);
+    }
+    public void StartFollowing()
+    {
+        StopAllCoroutines();
+
+        _enemyAnimator.SetTrigger(_nameDetectedParameter);
+    }
+    public void StopFollowing()
+    {
+        StopAllCoroutines();
+
+        _enemyAnimator.SetTrigger(_nameLostGoalParameter);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shoot() //it is necessary to choose button
     {
-        
+        StopAllCoroutines();
+        _enemyAnimator.SetTrigger(_nameCanShootParameter);
     }
 }
