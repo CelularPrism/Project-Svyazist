cc9423d9d0cfd1e2a83cbb74bacf4fc16f6fa590
LiubovPil <alexandraelle95@gmail.com>
2022-06-14 12:10:24 +0300

    Morze preparation

---
+++ Assets/Scripts/MiniGames/Morze/TextMorzeController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMorzeController : MonoBehaviour
+{
+    [Header("Health Settings")]
+    [SerializeField] private Image[] _mainImages;
+
+    [Header("Health Settings")]
+    [SerializeField] private Image[] _setImages;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
