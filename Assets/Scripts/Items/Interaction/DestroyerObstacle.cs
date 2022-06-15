1fc45cb61455d5510ec99e31463b261d58c46c26
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2022-06-15 19:55:58 +0700

    Fix problem

---
+++ Assets/Scripts/Items/Interaction/DestroyerObstacle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerObstacle : AbstractInteraction
+{
+    bool _destroy;
+    public override void Awake()
+    {
+        base.Awake();
+        _inputActions.Player.GetItem.performed += perf => DestroyObstacle();
+    }
+
+    private void DestroyObstacle()
+    {
+        if (_object != null)
+        {
+            _destroy = _playerInventory.GetItem(_object.key);
+            if (_destroy)
+            {
+                _object.Use();
+            }
+        }
+    }
+}
