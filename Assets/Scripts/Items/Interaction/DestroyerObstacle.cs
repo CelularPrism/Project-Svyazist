eccaaf33c8b0646254c28c9f180f248a8c9920b5
Wolfapredator <motocross.ru@mail.ru>
2022-06-27 11:46:05 +0300

    fix files

--- Assets/Scripts/Items/Interaction/DestroyerObstacle.cs
+++ Assets/Scripts/Items/Interaction/DestroyerObstacle.cs
@@ -23,10 +23,6 @@ public class DestroyerObstacle : AbstractInteraction
             if (_destroy)
             {
                 _object.Use(this);
-            } else
-            {
-                Debug.Log("Необходим " + _obstacle.nameItem);
-                ButtonsCue.SetActive(false);
             }
             else
             {
