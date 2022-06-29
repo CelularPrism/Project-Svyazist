df9a753be577232e71a1fff67220517fd7f259bf
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2022-06-29 19:05:07 +0700

    Create Localisation system

---
+++ Assets/Scripts/Localization/LocalisationSystem.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalisationSystem : MonoBehaviour
+{
+    public enum Language
+    {
+        English,
+        Russian
+    }
+
+
+    public static Language language = Language.English;
+
+    private static Dictionary<string, string> localisedEN;
+    private static Dictionary<string, string> localisedRU;
+
+    public static bool isInit;
+
+    public static void Init()
+    {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.LoadCSV();
+
+        localisedEN = csvLoader.GetDictionaryValues("en");
+        localisedRU = csvLoader.GetDictionaryValues("ru");
+        //var languages = Enum.GetValues(typeof(Language));
+        isInit = true;
+    }
+
+    public static string GetLocalisedValue(string key)
+    {
+        if (!isInit)
+        {
+            Init();
+        }
+
+        string value = key;
+        switch(language)
+        {
+            case Language.English:
+                localisedEN.TryGetValue(key, out value);
+                break;
+
+            case Language.Russian:
+                localisedRU.TryGetValue(key, out value);
+                break;
+        }
+        foreach (var val in localisedRU)
+        {
+            Debug.Log(val.Key + " " + val.Value);
+        }
+        return value;
+    }
+
+    public static Language GetLanguage()
+    {
+        return language;
+    }
+
+    public void SetLanguage(int num)
+    {
+        language = (Language)Enum.ToObject(typeof(Language), num);
+    }
+}
---
+++ Assets/Scripts/Localization/TextLocaliserUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextLocaliserUI : MonoBehaviour
+{
+    private TextMeshProUGUI textField;
+    
+    private LocalisationSystem.Language _language;
+
+    public string key;
+
+    private void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        _language = LocalisationSystem.GetLanguage();
+        string value = LocalisationSystem.GetLocalisedValue(key);
+        textField.text = value;
+    }
+
+    private void Update()
+    {
+        LocalisationSystem.Language lang = LocalisationSystem.GetLanguage();
+        if (_language != lang)
+        {
+            _language = lang;
+            string value = LocalisationSystem.GetLocalisedValue(key);
+            Debug.Log(value);
+            textField.text = value;
+        }
+    }
+}
